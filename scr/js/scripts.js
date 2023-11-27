c3ac481175f308f8091372dbf35d7c5ea7f24583
majudlorenzoni <juliamaria892@gmail.com>
2023-11-27 14:34:11 -0300

    Adição de luz e sombra nos objetos

--- scr/js/scripts.js
+++ scr/js/scripts.js
@@ -1,7 +1,10 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import * as dat from 'dat.gui';
 
-const renderer = new THREE.WebGLRenderer({antialias: true});
+const renderer = new THREE.WebGLRenderer();
+renderer.shadowMap.enabled = true;
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
@@ -23,32 +26,85 @@ camera.position.set(-10, 30, 30);
 orbit.update();
 
 const boxGeometry = new THREE.BoxGeometry();
-const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const boxMaterial = new THREE.MeshStandardMaterial({color: 0x6a5acd
+});
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
 
 const planeGeometry = new THREE.PlaneGeometry(30, 30);
-const planeMaterial = new THREE.MeshBasicMaterial(
+const planeMaterial = new THREE.MeshStandardMaterial(
     {color: 0xFFFFFF, 
     side: THREE.DoubleSide
 });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(plane);
+
 plane.rotation.x = Math.PI / 2;
+plane.receiveShadow = true;
+
 const gridHelper = new THREE.GridHelper(30);
 scene.add(gridHelper);
 
-const sphareGeometry = new THREE.SphereGeometry(4, 100, 100);
-const sphereMaterial = new THREE.MeshBasicMaterial({
+const sphereGeometry = new THREE.SphereGeometry(4, 50, 50);
+const sphereMaterial = new THREE.MeshStandardMaterial({
     color: 0x0000ff,
-    wireframe: true
+    wireframe: false
 });
-const sphere = new THREE.Mesh(sphareGeometry, sphereMaterial);
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 scene.add(sphere);
+
+sphere.position.set(-10, 10, 0);
+sphere.castShadow = true;
+
+const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 10);
+scene.add(directionalLight);
+directionalLight.position.set(-30, 50, 0);
+//directionalLight.castShadow = true;
+//directionalLight.shadow.camera.bottom = -12;
+
+//const dLighetHelper = new THREE.DirectionalLightHelper(directionalLight, 5);
+// scene.add(dLighetHelper);
+
+ const dLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+scene.add(dLightShadowHelper);
+const spotLight = new THREE.SpotLight(0xFFFFFF);
+scene.add(spotLight); 
+spotLight.position.set(-100, 100, 0);
+spotLight.castShadow = true; 
+
+const sLightHelper = new THREE.SpotLightHelper(spotLight);
+scene.add(sLightHelper);
+
+const gui = new dat.GUI();
+
+const options = {
+    sphereColor: '#ffea00',
+    wireframe: false,
+    speed: 0.01
+};
+
+gui.addColor(options, 'sphereColor').onChange(function(e){
+    sphere.material.color.set(e);
+});
+
+gui.add(options, 'wireframe').onChange(function(e){
+    sphere.material.wireframe = e;
+});
+
+gui.add(options, 'speed', 0, 0.1);
+
+let step = 0;
+
 
 function animate(time) {
     box.rotation.x = time / 1000;
     box.rotation.y = time / 1000;
+
+    step += options.speed;
+    sphere.position.y = 10 * Math.abs(Math.sin(step));
     renderer.render(scene, camera);
 }
 
