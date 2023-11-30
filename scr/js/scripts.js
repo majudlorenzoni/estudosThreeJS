08beb483cd50bcc3a513daaa106d910959edfd21
majudlorenzoni <juliamaria892@gmail.com>
2023-11-29 23:08:07 -0300

    Plano de fundo, movimento dos objetos e mudança nos objetos

--- scr/js/scripts.js
+++ scr/js/scripts.js
@@ -1,6 +1,10 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'dat.gui';
+import { CubeTextureLoader } from 'three';
+import estrelas from '../img/estrelas.jpg';
+import im2 from '../img/im2.jpg';
+
 
 const renderer = new THREE.WebGLRenderer();
 renderer.shadowMap.enabled = true;
@@ -15,7 +19,7 @@ const camera = new THREE.PerspectiveCamera(
     45,
     window.innerWidth / window.innerHeight,
     0.1,
-    1000
+    700
 );
 
 const orbit = new OrbitControls(camera, renderer.domElement);
@@ -26,7 +30,7 @@ camera.position.set(-10, 30, 30);
 orbit.update();
 
 const boxGeometry = new THREE.BoxGeometry();
-const boxMaterial = new THREE.MeshStandardMaterial({color: 0x6a5acd
+const boxMaterial = new THREE.MeshStandardMaterial({color: 0xff0d9a
 });
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
@@ -45,7 +49,7 @@ plane.receiveShadow = true;
 const gridHelper = new THREE.GridHelper(30);
 scene.add(gridHelper);
 
-const sphereGeometry = new THREE.SphereGeometry(4, 50, 50);
+const sphereGeometry = new THREE.SphereGeometry(4, 90, 90);
 const sphereMaterial = new THREE.MeshStandardMaterial({
     color: 0x0000ff,
     wireframe: false
@@ -53,37 +57,103 @@ const sphereMaterial = new THREE.MeshStandardMaterial({
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 scene.add(sphere);
 
-sphere.position.set(-10, 10, 0);
+sphere.position.set(10, 10, 0);
 sphere.castShadow = true;
+sphere.receiveShadow = true;
 
-const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+const ambientLight = new THREE.AmbientLight(0x333333);
 scene.add(ambientLight);
 
 const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 10);
 scene.add(directionalLight);
-directionalLight.position.set(-30, 50, 0);
-//directionalLight.castShadow = true;
-//directionalLight.shadow.camera.bottom = -12;
+directionalLight.position.set(30, 50, 0);
+directionalLight.castShadow = true;
+directionalLight.shadow.camera.bottom = -12;
 
-//const dLighetHelper = new THREE.DirectionalLightHelper(directionalLight, 5);
-// scene.add(dLighetHelper);
+const dLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5);
+scene.add(dLightHelper);
 
- const dLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
-scene.add(dLightShadowHelper);
-const spotLight = new THREE.SpotLight(0xFFFFFF);
+const dLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+scene.add(dLightShadowHelper); 
+
+const spotLight = new THREE.SpotLight(0xFFFFFF, 50000);
 scene.add(spotLight); 
 spotLight.position.set(-100, 100, 0);
 spotLight.castShadow = true; 
 
-const sLightHelper = new THREE.SpotLightHelper(spotLight);
-scene.add(sLightHelper);
+//scene.fog = new THREE.Fog(0xffffff, 1, 200);
+scene.fog = new THREE.FogExp2(0xFFFFFF, 0.01);
+
+//renderer.setClearColor(0xFFA8D9);
+
+const textureLoader = new THREE.TextureLoader();
+scene.background = textureLoader.load(estrelas);
+
+/* const cubeTextureLoader = new THREE.CubeTextureLoader();
+scene.background = cubeTextureLoader.load([
+    'im2.jpg', 'im2.jpg', 'estrelas.jpg', 'estrelas.jpg', 'estrelas.jpg', 'estrelas.jpg'
+]); */
+
+const box2Geometry = new THREE.BoxGeometry(4, 4, 4);
+const box2Material = new THREE.MeshBasicMaterial({
+   // map: textureLoader.load(im2)
+});
+const box2MultiMaterial = [
+    new THREE.MeshBasicMaterial({map: textureLoader.load(im2)}),
+    new THREE.MeshBasicMaterial({map: textureLoader.load(estrelas)}),
+    new THREE.MeshBasicMaterial({map: textureLoader.load(im2)}),
+    new THREE.MeshBasicMaterial({map: textureLoader.load(estrelas)}),
+    new THREE.MeshBasicMaterial({map: textureLoader.load(im2)}),
+    new THREE.MeshBasicMaterial({map: textureLoader.load(estrelas)}),
+];
+const box2 = new THREE.Mesh(box2Geometry, box2MultiMaterial);
+scene.add(box2);
+box2.position.set(0, 15, 10);
+//box.material.map = textureLoader.load(im2);
+
+const plane2Geometry = new THREE.PlaneGeometry(10, 10, 10, 10);
+const plane2Material = new THREE.MeshBasicMaterial({
+    color: 0xFFFFFF,
+    wireframe: true
+}); 
+const plane2 = new THREE.Mesh(plane2Geometry, plane2Material);
+scene.add(plane2);
+plane2.position.set(10, 10, 15);
+
+plane2.geometry.attributes.position.array[0] -= 10 * Math.random();
+plane2.geometry.attributes.position.array[1] -= 10 * Math.random();
+plane2.geometry.attributes.position.array[2] -= 10 * Math.random();
+const lastPointZ = plane2.geometry.attributes.position.array.length - 1;
+plane2.geometry.attributes.position.array[lastPointZ] -= 10 * Math.random();
+
+const sphere2Geometry = new THREE.SphereGeometry(4);
+/* const vShader = `
+    void main () {
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }`;
+
+const fShader = `
+    void main () {
+        gl_FragColor = vec4(0.5, 0.5, 1.0, 1.0);
+    }`;
+*/ 
+
+const sphere2Material = new THREE.ShaderMaterial({
+    vertexShader: document.getElementById('vertexShader').textContent,
+    fragmentShader: document.getElementById('fragmentShader').textContent,
+});
+
+const sphere2 = new THREE.Mesh(sphere2Geometry, sphere2Material);
+scene.add(sphere2);
+sphere2.position.set(-5, 10, 10);
 
 const gui = new dat.GUI();
+
 
 const options = {
     sphereColor: '#ffea00',
     wireframe: false,
-    speed: 0.01
+    speed: 0.01,
 };
 
 gui.addColor(options, 'sphereColor').onChange(function(e){
@@ -93,19 +163,54 @@ gui.addColor(options, 'sphereColor').onChange(function(e){
 gui.add(options, 'wireframe').onChange(function(e){
     sphere.material.wireframe = e;
 });
-
 gui.add(options, 'speed', 0, 0.1);
 
 let step = 0;
 
+const mousePosition = new THREE.Vector2();
 
+window.addEventListener('mousemove', function(e){
+    mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mousePosition.y = -(e.clientY / window.innerHeight) * 2 + 1;
+});
+
+const raycaster = new THREE.Raycaster();
+
+const sphereId = sphere.id;
+box2.name = 'theBox'
 function animate(time) {
     box.rotation.x = time / 1000;
     box.rotation.y = time / 1000;
 
     step += options.speed;
     sphere.position.y = 10 * Math.abs(Math.sin(step));
+
+    raycaster.setFromCamera(mousePosition, camera);
+    const intersects = raycaster.intersectObjects(scene.children);
+    console.log(intersects);
+
+    for (let i = 0; i < intersects.length; i++) {
+        if(intersects[i].object.id === sphereId)
+            intersects[i].object.material.color.set(0xFF0000);
+        if(intersects[i].object.name === 'theBox'){
+            intersects[i].object.rotation.x = time / 1000;
+            intersects[i].object.rotation.y = time / 1000;
+        }
+    }    
+    plane2.geometry.attributes.position.array[0] = 10 * Math.random();
+    plane2.geometry.attributes.position.array[1] = 10 * Math.random();
+    plane2.geometry.attributes.position.array[2] = 10 * Math.random();
+    plane2.geometry.attributes.position.array[lastPointZ] = 10 * Math.random();
+    plane2.geometry.attributes.position.needsUpdate = true;
+
     renderer.render(scene, camera);
+
 }
 
 renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', function(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
